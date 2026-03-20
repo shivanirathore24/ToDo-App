6ff166a318b9803758802d32a812fbd67a40c96d
shivanirathore24 <shivani.rathore2409@gmail.com>
2026-03-20 14:54:33 +0530

    Added: Converting ToDo App to Utility App

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-import { useState } from "react";
+import { Fragment } from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import TodoForm from "./components/ToDoForm/ToDoForm";
 import TodoList from "./components/ToDoList/ToDoList";
-import { Provider } from "react-redux";
-import {store} from "./redux/store";
+import NoteForm from "./components/NoteForm/NoteForm";
+import NoteList from "./components/NoteList/NoteList";
+import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  const createTodo = (text) => {
-    if (!text.trim()) return;
-    setTodos([...todos, { id: Date.now(), text, completed: false }]);
-  };
-
-  const toggleTodo = (index) => {
-    const list = [...todos];
-    list[index].completed = !list[index].completed;
-    setTodos(list);
-  };
-
   return (
-    <div className="app">
-      <h1>To Do App</h1>
-      <Provider store={store}>
-        <TodoForm onCreateTodo={createTodo} />
-        <TodoList todos={todos} onToggle={toggleTodo} />
-      </Provider>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route
+            path="/todo"
+            element={
+              <>
+                <h1>To Do</h1>
+                <TodoForm />
+                <TodoList />
+              </>
+            }
+          />
+
+          <Route
+            path="/notes"
+            element={
+              <>
+                <h1>Notes</h1>
+                <NoteForm />
+                <NoteList />
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
